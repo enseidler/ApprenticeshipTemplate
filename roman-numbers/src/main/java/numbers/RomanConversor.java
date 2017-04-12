d88fcbab1b3ddbbf6043087511d8b9c577eb7aa4
enseidler <eseidler@10pines.com>
2017-04-12 11:43:15 -0300

    RomanNumbers - Refactor en implementacion de conversion

--- roman-numbers/src/main/java/numbers/RomanConversor.java
+++ roman-numbers/src/main/java/numbers/RomanConversor.java
@@ -12,14 +12,10 @@ public class RomanConversor {
             return "IV";
         if(number == 9)
             return "IX";
-        if(number == 10)
-            return "X";
-        if(number == 11)
-            return "XI";
-        if(number == 12)
-            return "XII";
+        if(number > 9)
+            return "X" + StringUtils.repeat("I", number - 10);
         if(number > 4)
-            return "V" + StringUtils.repeat("I", number-5);
+            return "V" + StringUtils.repeat("I", number - 5);
         return StringUtils.repeat("I", number);
     }
 
