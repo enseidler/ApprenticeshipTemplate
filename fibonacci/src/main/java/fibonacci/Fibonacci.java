719ddb5f722ed1e2dd0d4205e455569a1264b92f
enseidler <eseidler@10pines.com>
2017-04-12 09:33:48 -0300

    Fibonacci - Refactor en la verificacion de posicion negativa

--- fibonacci/src/main/java/fibonacci/Fibonacci.java
+++ fibonacci/src/main/java/fibonacci/Fibonacci.java
@@ -8,13 +8,17 @@ import fibonacci.errors.PosicionNegativaException;
 public class Fibonacci {
 
     public static Integer valorEnPosicion(Integer posicion) throws Exception {
-        if (posicion < 0)
-            throw new PosicionNegativaException();
+        verificarPosicion(posicion);
 
         if(posicion > 1)
             return valorEnPosicion(posicion-1) + valorEnPosicion(posicion-2);
 
         return posicion;
+    }
+
+    private static void verificarPosicion(Integer posicion) throws PosicionNegativaException {
+        if (posicion < 0)
+            throw new PosicionNegativaException();
     }
 
 }
