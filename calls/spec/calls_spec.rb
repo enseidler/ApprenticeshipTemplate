3d124d13cf10ac634950da407c4c1a870524e817
enseidler <eseidler@10pines.com>
2017-05-02 13:58:51 -0300

    Llamadas - Refactor de tests - Eliminacion de tests concretos redundantes

--- calls/spec/calls_spec.rb
+++ calls/spec/calls_spec.rb
@@ -6,16 +6,6 @@ describe 'Phone Biller' do
   let(:phone_biller) { PhoneBiller.new }
 
   context 'cuando la llamada es nacional' do
-    it '1 minuto cuesta 30 centavos' do
-      a_national_call = 1
-      expect(phone_biller.call_cost(a_national_call)).to eq 0.30
-    end
-
-    it '2 minutos cuestan 60 centavos' do
-      a_national_call = 2
-      expect(phone_biller.call_cost(a_national_call)).to eq 0.60
-    end
-
     it 'cuesta 30 centavos por cada minuto' do
       a_national_call = 15
       expect(phone_biller.call_cost(a_national_call)).to eq 4.50
