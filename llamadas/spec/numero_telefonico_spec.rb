5878bba2ffc4ecdcfdf838acb299679ae9ea070a
enseidler <eseidler@10pines.com>
2017-05-04 16:18:43 -0300

    Llamadas - Test07 - Test numero telefonico de que no es de Sudamerica

--- llamadas/spec/numero_telefonico_spec.rb
+++ llamadas/spec/numero_telefonico_spec.rb
@@ -11,4 +11,9 @@ describe 'Numero Telefonico' do
     expect(factory.numero_colombia.sudamerica?).to be_truthy
   end
 
+  it 'cuando el codigo de pais partenece a un pais de otra region que NO es sudamerica entonces NO es un numero de sudamerica' do
+    expect(factory.numero_espanha.sudamerica?).to be_falsey
+    expect(factory.numero_usa.sudamerica?).to be_falsey
+  end
+
 end
