78b3026af386cf787a56319629c086be1f0c4616
enseidler <eseidler@10pines.com>
2017-05-05 08:55:30 -0300

    Llamadas - Refactor de tests y facturador de llamadas

--- llamadas/spec/llamada_spec.rb
+++ llamadas/spec/llamada_spec.rb
@@ -2,53 +2,49 @@ require 'rspec'
 require_relative '../lib/llamada'
 require_relative '../lib/numero_telefonico'
 require_relative '../lib/factory_de_numeros_telefonicos'
+require_relative '../lib/facturador_de_llamada'
 
 describe 'Llamada' do
 
   let(:factory) { FactoryDeNumerosTelefonicos.new }
+  let(:facturador_de_llamada) { FacturadorDeLlamada.new }
 
   context 'cuando la llamada es nacional' do
     it 'cuesta 30 centavos por minuto' do
       una_llamada_nacional = Llamada.new(14,
                                          factory.numero_bsas,
                                          factory.numero_argentina)
-      expect(una_llamada_nacional.costo).to eq 4.20 # dinero
+      expect(facturador_de_llamada.costo_de_llamada(una_llamada_nacional)).to eq 4.20 # dinero
     end
   end
 
-  context 'cuando la llamada es a Europa' do
-    it 'cuesta 70 centavos por minuto' do
+  context 'cuando la llamada es internacional' do
+    it 'cuesta 70 centavos el minuto a Europa' do
       una_llamada_a_europa = Llamada.new(20,
                                          factory.numero_bsas,
                                          factory.numero_espanha)
-      expect(una_llamada_a_europa.costo).to eq 14.00 # dinero
+      expect(facturador_de_llamada.costo_de_llamada(una_llamada_a_europa)).to eq 14.00 # dinero
     end
-  end
 
-  context 'cuando la llamada es a Norte America' do
-    it 'cuesta 70 centavos por minuto' do
+    it 'cuesta 70 centavos el minuto a Norte America' do
       una_llamada_a_norte_america = Llamada.new(20,
                                                 factory.numero_bsas,
                                                 factory.numero_usa)
-      expect(una_llamada_a_norte_america.costo).to eq 14.00 # dinero
+      expect(facturador_de_llamada.costo_de_llamada(una_llamada_a_norte_america)).to eq 14.00 # dinero
     end
-  end
 
-  context 'cuando la llamada es a Sudamerica' do
-    it 'cuesta 50 centavos por minuto' do
+    it 'cuesta 50 centavos el minuto a Sudamerica' do
       una_llamada_a_sudamerica = Llamada.new(15,
                                              factory.numero_bsas,
                                              factory.numero_colombia)
-      expect(una_llamada_a_sudamerica.costo).to eq 7.50 # dinero
+      expect(facturador_de_llamada.costo_de_llamada(una_llamada_a_sudamerica)).to eq 7.50 # dinero
     end
-  end
 
-  context 'cuando la llamada es a un pais del resto del mundo' do
-    it 'cuesta 1.50 pesos por minuto' do
+    it 'cuesta 1.50 pesos el minuto al resto del mundo' do
       una_llamada_a_asia = Llamada.new(10,
                                        factory.numero_bsas,
                                        factory.numero_china)
-      expect(una_llamada_a_asia.costo).to eq 15.00 # dinero
+      expect(facturador_de_llamada.costo_de_llamada(una_llamada_a_asia)).to eq 15.00 # dinero
     end
   end
 
