a732e2f23da9d209867b671101320b468707bd57
enseidler <eseidler@10pines.com>
2017-05-11 10:33:53 -0300

    HolidayCalendar - Test de persistencia de un calendario con regla de feriado de dia de mes

--- holiday-calendar/src/test/java/calendar/persistence/PersistenceHolidayCalendarTest.java
+++ holiday-calendar/src/test/java/calendar/persistence/PersistenceHolidayCalendarTest.java
@@ -1,6 +1,7 @@
 package calendar.persistence;
 
 import calendar.model.HolidayCalendar;
+import calendar.model.HolidayRuleDayOfMonth;
 import calendar.model.HolidayRuleDayOfWeek;
 import calendar.services.HolidayCalendarService;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.MonthDay;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -37,6 +39,21 @@ public class PersistenceHolidayCalendarTest {
 
         assertTrue(persistedHolidayCalendar.isHoliday(aFriday));
         assertFalse(persistedHolidayCalendar.isHoliday(aSaturday));
+    }
+
+    @Test
+    public void persistingAHolidayCalendarWithAHolidayRuleDayOfMonth() {
+        HolidayCalendar holidayCalendar = new HolidayCalendar();
+        holidayCalendar.addHolidayRule(new HolidayRuleDayOfMonth(MonthDay.of(5, 1)));
+
+        LocalDate aMayFirst = LocalDate.of(2017, 5, 1);
+        LocalDate aMaySeventh = LocalDate.of(2017, 5, 7);
+
+        Long id = holidayCalendarService.save(holidayCalendar);
+        HolidayCalendar persistedHolidayCalendar = holidayCalendarService.findById(id);
+
+        assertTrue(persistedHolidayCalendar.isHoliday(aMayFirst));
+        assertFalse(persistedHolidayCalendar.isHoliday(aMaySeventh));
     }
 
 }
