7a91c7b7cf9f7f0a04d498b2384bed9a947a6fcf
enseidler <eseidler@10pines.com>
2017-06-08 11:09:05 -0300

    holiday-calendar-react - Validacion de dia de mes

--- holiday-calendar-react/src/components/HolidayRuleDayOfMonthForm.js
+++ holiday-calendar-react/src/components/HolidayRuleDayOfMonthForm.js
@@ -4,12 +4,17 @@ import {holidayFormStore} from "../stores/index"
 import * as types from "../actions/actionTypes"
 export class HolidayRuleDayOfMonthForm extends Component {
 
+    months = ['Enero', 'Febrero', 'Marzo', 'Abril',
+    'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+    'Octubre', 'Noviembre', 'Diciembre'];
+
+
+    daysOfMonth() {
+        var selectedMonth = holidayFormStore.getState().month;
+        return new Date(new Date().getFullYear(), selectedMonth, 0).getDate();
+    }
+
     render() {
-
-        var months = ['Enero', 'Febrero', 'Marzo', 'Abril',
-            'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
-            'Octubre', 'Noviembre', 'Diciembre'];
-
         return (
             <div className="row">
                 <div className="col-md-6">
@@ -21,22 +26,26 @@ export class HolidayRuleDayOfMonthForm extends Component {
                 <div className="col-md-4">
                     <select
                         className="custom-select form-control"
-                        value={months[holidayFormStore.getState().month-1]} onChange={(event)=>
+                        value={this.months[holidayFormStore.getState().month-1]} onChange={(event)=>
                         holidayFormStore.dispatch(
                             {
                                 type:types.CHANGE_MONTH,
-                                month:months.indexOf(event.target.value)+1
+                                month:this.months.indexOf(event.target.value)+1
                             })} >
-                        {months.map(day =>
+                        {this.months.map(day =>
                             <option>{day}</option>
                         )}
                     </select>
                 </div>
                 <div className="col-md-2">
-                    <input type="text" className="form-control" size={"1"}
-                           placeholder="Dia"
-                           value={holidayFormStore.getState().day_of_month_holiday}
-                            onChange={(event)=>
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Dia"
+                        value={holidayFormStore.getState().day_of_month_holiday}
+                        min={1}
+                        max={this.daysOfMonth()}
+                        onChange={(event)=>
                             holidayFormStore.dispatch(
                                 {
                                     type:types.CHANGE_DAY_OF_MONTH,
